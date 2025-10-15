680751bc91f4b3592ff0db4a65a9551e73cac911
leleah <alya.leah.13@gmail.com>
2025-10-15 13:32:36 +0300

    fear:sin 1000 points

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
+#include <math.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
 #define I2C_BUS "/dev/i2c-1"
 #define MCP4725_ADDR 0x60
+#define PI 3.14159
 int main()
 {
 int fd;
+int value,angle;
 fd=open(I2C_BUS,O_RDWR);
         if(fd<0)
         {
@@ -24,4 +28,20 @@ fd=open(I2C_BUS,O_RDWR);
         }
 
 printf("Connected to mcp4725 (0x%02X)\r\n",MCP4725_ADDR);
+
+int N=1000;
+for(int k=0; k<N-1; k++)
+{
+    angle=2*M_PI*k/N;
+    value=(sin(angle)+1.0)/2.0*4095;
+    uint8_t ob = (value>>4)&0xFF;
+    uint8_t sb = (value&0x0F)<<4;
+    uint8_t buff[2] = { ob, sb };
+    write(fd,buff,2);
+    usleep(100);
+    if(k==N)
+    {
+        k=0;
+    }
+}
 }
